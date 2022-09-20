d8faa1a3fe1d78f5d14654e2a704a84ed3af93cb
ArkPal <arkadiusz.palczak@gmail.com>
2022-09-20 11:53:26 +0200

    First test

---
+++ cypress/e2e/contact_us.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress"/>
+
+describe("Test Contact Us form on WebdriverUni", () => {
+    it("Should be able to submit a successful submission via contact us form", () => {
+        cy.visit(
+            "https://www.webdriveruniversity.com/Contact-Us/contactus.html"
+        );
+        cy.get('[name = "first_name"]').type("John");
+        cy.get('[name="last_name"]').type("Doe");
+        cy.get('[name="email"]').type("johndoe@dummymail.com");
+        cy.get("textarea.feedback-input").type("New comment");
+        cy.get('[type="submit"]').click();
+    });
+});
