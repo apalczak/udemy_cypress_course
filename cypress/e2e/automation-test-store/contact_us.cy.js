930295abd24c32d5e6d06ce4028ee4d19e302a45
ArkPal <arkadiusz.palczak@gmail.com>
2022-09-22 08:46:40 +0200

    CSS selectors exercise

--- cypress/e2e/automation-test-store/contact_us.cy.js
+++ cypress/e2e/automation-test-store/contact_us.cy.js
@@ -3,10 +3,14 @@
 describe("Test Contact Us form on Automation Test Store", () => {
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit("https://www.automationteststore.com/");
-        cy.xpath(`//a[contains(@href,'contact')]`).click();
+        cy.get(`a[href$="contact"]`).click();
         cy.get("#ContactUsFrm_first_name").type("John");
         cy.get("#ContactUsFrm_email").type("johndoe@dummymail.com");
         cy.get("#ContactUsFrm_enquiry").type("New enquiry");
-        cy.get(".col-md-6 > .btn").click();
+        cy.get(`button[title="Submit"]`).click();
+        cy.get(".mb40 > :nth-child(3)").should(
+            "have.text",
+            "Your enquiry has been successfully sent to the store owner!".trim()
+        );
     });
 });
--- cypress/e2e/webdriver-uni/contact_us.cy.js
+++ cypress/e2e/webdriver-uni/contact_us.cy.js
@@ -1,6 +1,12 @@
 /// <reference types="cypress"/>
 
 describe("Test Contact Us form on WebdriverUni", () => {
+    it.only("Should have a charset set to UTF-8", () => {
+        cy.visit(
+            "https://www.webdriveruniversity.com/Contact-Us/contactus.html"
+        );
+        cy.document().should("have.property", "charset").and("eq", "UTF-8");
+    });
     it("Should be able to submit a successful submission via contact us form", () => {
         cy.visit(
             "https://www.webdriveruniversity.com/Contact-Us/contactus.html"
@@ -10,6 +16,17 @@ describe("Test Contact Us form on WebdriverUni", () => {
         cy.get('[name="email"]').type("johndoe@dummymail.com");
         cy.get("textarea.feedback-input").type("New comment");
         cy.get('[type="submit"]').click();
+        cy.get("h1").should("have.text", "Thank You for your Message!");
     });
-    it("Should not be able to submit a successful submission as all fields are required", () => {});
+    it("Should not be able to submit a successful submission as all fields are required", () => {
+        cy.visit(
+            "https://www.webdriveruniversity.com/Contact-Us/contactus.html"
+        );
+        cy.get('[name = "first_name"]').type("John");
+        cy.get('[name="last_name"]').type("Doe");
+        // cy.get('[name="email"]').type("johndoe@dummymail.com");
+        cy.get("textarea.feedback-input").type("New comment");
+        cy.get('[type="submit"]').click();
+        cy.get("body").contains(`Error: Invalid email address`);
+    });
 });
