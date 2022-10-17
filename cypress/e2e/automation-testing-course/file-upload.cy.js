58ee8605491bd8472a800adfe7430138c508c2da
ArkPal <arkadiusz.palczak@gmail.com>
2022-10-17 10:42:55 +0200

    File upload exercise

---
+++ cypress/e2e/automation-testing-course/file-upload.cy.js
@@ -0,0 +1,25 @@
+///<reference types="cypress"/>
+
+describe("Uploading files", () => {
+    it("Upload a file", () => {
+        cy.visit("http://www.webdriveruniversity.com");
+        cy.get("#file-upload")
+            .invoke("removeAttr", "target")
+            .click({ force: true });
+        cy.get("#myFile").selectFile("cypress/fixtures/laptop.png");
+        cy.get("#submit-button").click();
+        cy.on("window:alert", (str) => {
+            expect(str).to.equal("Your file has now been uploaded!");
+        });
+    });
+    it("Upload no file", () => {
+        cy.visit("http://www.webdriveruniversity.com");
+        cy.get("#file-upload")
+            .invoke("removeAttr", "target")
+            .click({ force: true });
+        cy.get("#submit-button").click();
+        cy.on("window:alert", (str) => {
+            expect(str).to.equal("You need to select a file to upload!");
+        });
+    });
+});
